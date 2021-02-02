56f8f91033baf5ea889278879494e62ed0611e34
prakashraj <kprakashraj541@gmail.com>
2021-02-02 13:52:32 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let dataList = [
+    {
+        "name": "Tucker Peck",
+        "description": "Sint do esse tempor esse elit eu reprehenderit mollit velit id laborum nostrud. Veniam velit consequat enim do eu amet et ad officia quis.",
+        "picture": "http://placehold.it/512x256"
+    },
+    {
+        "name": "Morse Mcmahon",
+        "description": "Do eu aliquip nulla nostrud. Lorem duis incididunt mollit exercitation do eu.",
+        "picture": "http://placehold.it/512x256"
+    },
+    {
+        "name": "Livingston Ellis",
+        "description": "Do proident commodo excepteur eiusmod qui incididunt sint consectetur Lorem minim. Quis qui incididunt veniam minim ullamco magna magna esse tempor in eu amet minim aliquip.",
+        "picture": "http://placehold.it/512x256"
+    },
+    {
+        "name": "Kendra Holman",
+        "description": "Sunt non aliqua do nostrud tempor. In elit enim mollit reprehenderit ipsum.",
+        "picture": "http://placehold.it/512x256"
+    },
+    {
+        "name": "Olson Dean",
+        "description": "Nostrud cupidatat minim sunt commodo irure exercitation eu irure irure tempor laborum aliquip sunt esse. Culpa sint veniam adipisicing veniam est sit enim cupidatat magna incididunt id in ullamco.",
+        "picture": "http://placehold.it/512x256"
+    },
+    {
+        "name": "Gillespie Waters",
+        "description": "Dolor pariatur deserunt occaecat pariatur culpa in officia. Amet ex id esse amet et voluptate ea ullamco sunt.",
+        "picture": "http://placehold.it/512x256"
+    }
+]
